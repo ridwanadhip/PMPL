1713730c4196af52a98c512d90fa84816f7a5d29
ridwan.adhi <ridwan.adhi@ui.ac.id>
2015-09-10 04:19:39 +0000

    Refactor halaman beranda

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
-'''
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'To-Do' in browser.title
-browser.quit()
-'''
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -17,10 +11,28 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def test_website(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
-		# self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+		
+		self.fail('Finish the test!')
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,9 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
 
-# Create your tests here.
-# class SmokeTest(TestCase):
-# 	def test_bad_maths(self):
-# 		self.assertEqual(1+1, 3)
 
 class HomePageTest(TestCase):
 	def test_url_resolvers(self):
@@ -17,6 +13,8 @@ class HomePageTest(TestCase):
 	def test_home_page(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>\n'))
+		expected_html = render_to_string('index.html')
+		self.assertEqual(response.content.decode(), expected_html)
+		# self.assertTrue(response.content.startswith(b'<html>'))
+		# self.assertIn(b'<title>To-Do lists</title>', response.content)
+		# self.assertTrue(response.content.strip().endswith(b'</html>'))
