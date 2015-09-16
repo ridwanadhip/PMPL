9df6389183d47c343c507e0358f27292a5b20fa0
ridwan.adhi <ridwan.adhi@ui.ac.id>
2015-09-16 08:21:49 +0000

    Menambahkan pengujian untuk memeriksa apakah teks berhasil dimasukkan ke tabel atau tidak

--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-	# return HttpResponse('<html><title>To-Do lists</title></html>')
-	return render(request, 'index.html')
+	# if request.method == 'POST':
+	#	return HttpResponse(request.POST['item_text'])
+	
+	data = {
+		'new_item_text': request.POST.get('item_text', ''),
+	}
+
+	return render(request, 'index.html', data)
+
