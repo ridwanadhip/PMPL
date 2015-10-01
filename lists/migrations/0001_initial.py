b3ec16d8b3a807a7de407cd379dd242599624642
RIDWAN ADHI PRATAMA - 1206250185 <ridwan.adhi@bulan.cs.ui.ac.id>
2015-10-01 10:36:54 +0700

    tahap pertama pembuatan functional test untuk pengujian layout

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -13,14 +13,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
                 ('text', models.TextField(default='')),
             ],
         ),
         migrations.CreateModel(
             name='List',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
             ],
         ),
         migrations.AddField(
