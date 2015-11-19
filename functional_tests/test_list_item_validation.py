fa3addcccdca9779ee803859c2635ac207dbb0ca
Ubuntu <ridwan@pmpl7ridwan.pmpl7ridwan.i5.internal.cloudapp.net>
2015-11-19 04:07:59 +0000

    Menambahkan test untuk pembuatan list yang kosong

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,18 +6,18 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
         
-        error = self.browser.find_element_css_selector('.has-error')
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
         
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_table('1: Buy milk')
         
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
         
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
-        self.check_for_row_in_list_table('1: Buy milk')
-        self.check_for_row_in_list_table('2: Make tea')
+        self.check_table('1: Buy milk')
+        self.check_table('2: Make tea')
